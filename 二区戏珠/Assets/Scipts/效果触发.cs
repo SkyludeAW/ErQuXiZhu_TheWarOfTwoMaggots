80ebd4d6c61d35793085782545daa2ba934dfc3d
Martin-aaa <ruichem2@uci.edu>
2026-04-13 23:11:00 -0700

    改了个名字

--- 二区戏珠/Assets/Scipts/效果触发.cs
+++ 二区戏珠/Assets/Scipts/效果触发.cs
@@ -1,29 +1,35 @@
 using System;
 using UnityEngine;
 
-public class 随机效果 : MonoBehaviour
+public class 鏁堟灉瑙﹀彂 : MonoBehaviour
 {
     private float RotationSpeed = 100f;
     private float DestroyTime = 5f;
-    public static Action<随机效果> action;
-
+    public static Action<鏁堟灉瑙﹀彂> action;
     void Start()
     {
         Destroy(gameObject, DestroyTime);
+     
     }
 
-
+    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * RotationSpeed * Time.deltaTime,Space.World);
+        transform.Rotate(Vector3.up * RotationSpeed * Time.deltaTime, Space.World);
     }
+
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(gameObject,0.1f);
-                action?.Invoke(this);
+            Destroy(gameObject, 0.1f);
+            action?.Invoke(this);
         }
     }
 }
+
+    
+
+
+
